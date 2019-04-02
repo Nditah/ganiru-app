d44b96f4393205c799d011be53eacc3eee14dfc7
MAC <mac@MACs-MacBook-Pro.local>
2019-04-02 18:45:46 +0100

    Template

---
+++ src/app/examples/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit, OnDestroy {
+
+    data: Date = new Date();
+    focus;
+    focus1;
+
+    constructor() { }
+
+    ngOnInit() {
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.add('login-page');
+
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.add('navbar-transparent');
+    }
+
+    ngOnDestroy() {
+        var body = document.getElementsByTagName('body')[0];
+        body.classList.remove('login-page');
+
+        var navbar = document.getElementsByTagName('nav')[0];
+        navbar.classList.remove('navbar-transparent');
+    }
+
+}
